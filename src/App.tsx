ca1350c234f4de5a2006045947a780be26603673
Bas Mulder <bas.d.mulder@gmail.com>
2024-01-13 15:18:01 +0100

    setup gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,37 @@
 import "./App.css";
-import { Accidental, BaseNote, NoteList } from "./classes/Note";
+import {
+  Accidental,
+  BaseNote,
+  CompareAccententals,
+  Note,
+  NoteList,
+} from "./classes/Note";
+import { groupBy } from "./extensions/groupBy";
 
 function App() {
-  const note = NoteList.find((n) => n.stringNotation === "A4")!!;
+  // NoteList.sort(
+  //   (a: Note, b: Note) =>
+  //     b.octave - a.octave ||
+  //     a.note.localeCompare(b.note) ||
+  //     CompareAccententals(a.accidental, b.accidental)
+  // );
+  const groupedNotes = groupBy(NoteList, "octave");
 
-  let stopContext: () => void;
-  let stoppable: boolean = false;
-
-  const playNote = () => {
+  const playNote = (note: Note) => {
     note.playNote(1);
   };
 
   return (
     <div className="App">
-      <button onClick={playNote}>Toggle Note</button>
+      {Object.values(groupedNotes).map((notes: Note[]) => (
+        <div style={{ display: "block" }} key={notes[0].octave}>
+          {notes.map((note: Note) => (
+            <button key={note.stringNotation} onClick={() => playNote(note)}>
+              {note.stringNotation}
+            </button>
+          ))}
+        </div>
+      ))}
     </div>
   );
 }
